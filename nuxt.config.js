4b4cfa1bd8f4100abd77a43a2f61ca1aa9e43758
kenwoo9y <ei.1px.9.01@gmail.com>
2023-03-26 14:20:46 +0900

    add original color theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,6 +47,21 @@ export default {
     // baseURL: 'http://localhost:3000'
   },
 
+  vuetify: {
+    theme: {
+      themes: {
+        light: {
+          primary: '4080BE',
+          info: '4FC1E9',
+          success: '44D69E',
+          warning: 'FEB65E',
+          error: 'FB8678',
+          background: 'f6f6f4'
+        }
+      }
+    }
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }

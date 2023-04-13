45b6154ea4efc72ec92c613cfc549254ee8a27ac
kenwoo9y <ei.1px.9.01@gmail.com>
2023-04-13 23:16:50 +0900

    compatible with responsive design

--- components/home/HomeAppBar.vue
+++ components/home/HomeAppBar.vue
@@ -9,13 +9,13 @@
     <app-logo 
       @click.native="$vuetify.goTo('#scroll-top')"
     />
-    <v-toolbar-title>
+    <v-toolbar-title class="hidden-mobile-and-down">
       {{ appName }}
     </v-toolbar-title>
 
     <v-spacer />
 
-    <v-toolbar-items class="ml-2" absolute right>
+    <v-toolbar-items class="ml-2 hidden-ipad-and-down" absolute right>
       <v-btn
         v-for="(menu, i) in menus"
         :key="`menu-btn-${i}`"
@@ -28,6 +28,36 @@
 
     <app-signin-button />
     <app-signup-button />
+
+    <v-menu
+      bottom
+      nudge-left="110"
+      nudge-width="100"
+    >
+      <template 
+        v-slot:activator="{ on }"
+      >
+        <v-app-bar-nav-icon
+          class="hidden-ipad-and-up"
+          v-on="on"
+        />
+      </template>
+      <v-list
+        dense
+        class="hidden-ipad-and-up"
+      >
+        <v-list-item
+          v-for="(menu, i) in menus"
+          :key="`menu-list-${i}`"
+          exact
+          @click="goTo(`#${menu.title}`)"
+        >
+          <v-list-item-title>
+            {{ $t(`menus.${menu.title}`) }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 

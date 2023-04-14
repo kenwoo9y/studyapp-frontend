3cdeec8fb699281a2ea1ee2ee31660ea2a56e678
kenwoo9y <ei.1px.9.01@gmail.com>
2023-04-14 23:55:10 +0900

    fix footer

--- components/app/AppFooter.vue
+++ components/app/AppFooter.vue
@@ -1,16 +1,37 @@
 <template>
-  <div>
+  <div :style="{ marginTop: `${height}px`}">
     <v-footer
       absolute
       dark
       color="black"
+      :height="height"
     >
-      AppFooter.vue
+      <v-col
+        cols="12"
+        class="py-0"
+      >
+        <div class="text-center text-body-2">
+          &copy;{{ copyRightYear }}
+          <strong>{{ appName }}</strong>
+        </div>
+      </v-col>
     </v-footer>
   </div>
 </template>
 
 <script>
 export default {
+  data ({ $config: { appName } }) {
+    return {
+      appName,
+      height: 32
+    }
+  },
+  computed: {
+    copyRightYear () {
+      const thisYear = new Date().getFullYear()
+      return thisYear
+    }
+  }
 }
 </script>

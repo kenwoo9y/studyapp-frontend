49b78bb4f0d5b84d821d042f4e1d170c01041a6c
kenwoo9y <ei.1px.9.01@gmail.com>
2023-04-15 20:30:10 +0900

    componentize Toolbar

--- components/home/HomeAppBar.vue
+++ components/home/HomeAppBar.vue
@@ -9,9 +9,9 @@
     <app-logo 
       @click.native="$vuetify.goTo('#scroll-top')"
     />
-    <v-toolbar-title class="hidden-mobile-and-down">
-      {{ appName }}
-    </v-toolbar-title>
+    <app-title 
+      class="hidden-mobile-and-down"
+    />
 
     <v-spacer />
 
@@ -74,9 +74,8 @@ export default {
       default: 0
     }
   },
-  data ({ $config: { appName }, $store }) {
+  data ({ $store }) {
     return {
-      appName,
       scrollY: 0,
       appBarHeight: $store.state.styles.beforeLogin.appBarHeight
     }

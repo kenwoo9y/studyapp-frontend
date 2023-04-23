fe8c98604c91590757975e505420563b955d59be
kenwoo9y <ei.1px.9.01@gmail.com>
2023-04-23 23:26:29 +0900

    update signup.vue

--- pages/signup.vue
+++ pages/signup.vue
@@ -4,6 +4,7 @@
       #user-form-card-content
     >
       <v-form 
+        ref="form"
         v-model="isValid"
       >
         <user-form-name 
@@ -18,10 +19,12 @@
           set-validation
         />
         <v-btn 
-          :disabled="!isValid"
+          :disabled="!isValid || loading"
+          :loading="loading"
           block
           color="primary"
           class="white--text"
+          @click="signup"
         >
          登録
         </v-btn>
@@ -36,7 +39,21 @@ export default {
   data() {
     return {
       isValid: false,
+      loading: false,
       params: { user: { name: '', email: '', password: '' } }
+    }
+  },
+  methods: {
+    signup() {
+      this.formReset()
+      this.loading = "true"
+      setTimeout(() => (this.loading = false), 1500)
+    }
+  },
+  formReset() {
+    this.$refs.form
+    for (const key in this.params.user) {
+      this.params.user[key] = ''
     }
   }
 }

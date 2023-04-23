814d1a89d2ae6bccda086a69eff649806f1a831a
kenwoo9y <ei.1px.9.01@gmail.com>
2023-04-23 23:26:05 +0900

    update signin.vue

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,11 +1,61 @@
 <template>
-  <div>
-    signin.vue
-  </div>
+  <user-form-card>
+    <template 
+      #user-form-card-content
+    >
+      <v-form 
+        ref="form"
+        v-model="isValid"
+      >
+        <user-form-email 
+          :email.sync="params.user.email"
+          placeholder
+        />
+        <user-form-password 
+          :password.sync="params.user.password"
+        />
+        <v-card-actions>
+          <nuxt-link 
+            to="#" 
+            class="body-2 text-decoration-none"
+          >
+            パスワードを忘れた場合はこちら
+          </nuxt-link>
+        </v-card-actions>
+        <v-card-text 
+          class="px-0"
+        >
+          <v-btn 
+            :disabled="!isValid || loading"
+            :loading="loading"
+            block
+            color="primary"
+            class="white--text"
+            @click="signin"
+          >
+           ログイン
+          </v-btn>
+        </v-card-text>
+      </v-form>
+    </template>
+  </user-form-card>
 </template>
 
 <script>
 export default {
-  layout: 'before-login'
+  layout: 'before-login',
+  data() {
+    return {
+      isValid: false,
+      loading: false,
+      params: { user: { email: '', password: '' } }
+    }
+  },
+  methods: {
+    signin() {
+      this.loading = "true"
+      setTimeout(() => (this.loading = false), 1500)
+    }
+  }
 }
 </script>

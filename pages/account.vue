f721634367b3aa4d3ed419c8ea6bc7715b5ba09b
kenwoo9y <ei.1px.9.01@gmail.com>
2023-04-30 23:49:09 +0900

    add account page

---
+++ pages/account.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <nuxt-child />
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'signed-in',
+  validate ({ route }) {
+    return route.name !== 'account'
+  }
+}
+</script>
